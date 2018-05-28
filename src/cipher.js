6bc6b293f88139f899ec05a1d7d8f473c8e78027
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-05-27 23:12:35 -0400

    JS funcion e index de codificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,27 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, message) => {
+    const charaEncode = message.length;
+    console.log(message);
+    console.log(offset);
+
+    let finalMessage = '';
+    let letterEncode = 0;
+    let funEncode = 0;
+    let letterResEncode = 0;
+
+    for (let i = 0; i < charaEncode; i++) {
+      letterEncode = message.charCodeAt(i);
+      console.log('a', letterEncode);
+      funEncode = 0;
+      funEncode = ((letterEncode - 65 + offset) % 26 + 65);
+      console.log('b', offset);
+      console.log('c', funEncode);
+      letterResEncode = String.fromCharCode(funEncode);
+      finalMessage = finalMessage + letterResEncode;
+    }
+    console.log(finalMessage);
+    return finalMessage;
+
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-/* Acá va tu código */
+window.onload = () => {
+
+    const encodeBtn = document.getElementById('btnEncode');
+
+    encodeBtn.addEventListener('click', () => {
+        const stringEncode = document.getElementById('textAreaEncode').value;
+        const offsetMy = parseInt(document.getElementById('myOffset').value);
+        const sum = window.cipher.encode(offsetMy, stringEncode);
+    })
+
+}

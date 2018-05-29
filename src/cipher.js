9165c6452d04bd22d480e0474eaf6ef571606fd6
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-05-28 21:59:02 -0400

    App Funcional con reconocimiento de minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -2,14 +2,17 @@ window.cipher = {
 
   encode: (offsetEncode, stringEncode) => {
 
-    const charaEncode = stringEncode.length;
+    console.log(stringEncode);
+    stringMayEncode = stringEncode.toUpperCase();
+    console.log(stringMayEncode);
+    const charaEncode = stringMayEncode.length;
     let finalMessageEncode = '';
     let letterEncode = 0;
     let funEncode = 0;
     let letterResEncode = 0;
 
     for (let i = 0; i < charaEncode; i++) {
-      letterEncode = stringEncode.charCodeAt(i);
+      letterEncode = stringMayEncode.charCodeAt(i);
       funEncode = 0;
       funEncode = ((letterEncode - 65 + offsetEncode) % 26 + 65);
       letterResEncode = String.fromCharCode(funEncode);
@@ -20,16 +23,25 @@ window.cipher = {
 
   decode: (offsetDecode, stringDecode) => {
 
-    const charaDecode = stringDecode.length;
+    console.log(stringDecode);
+    stringMayDecode = stringDecode.toUpperCase();
+    console.log(stringMayDecode);
+    const charaDecode = stringMayDecode.length;
     let finalMessageDecode = '';
     let letterDecode = 0;
     let funDecode = 0;
     let letterResDecode = 0;
 
     for (let j = 0; j < charaDecode; j++) {
-      letterDecode = stringDecode.charCodeAt(j);
+      letterDecode = stringMayDecode.charCodeAt(j);
       funDecode = 0
-      funDecode = ((letterDecode - 65 - offsetDecode) % 26 + 65);
+
+      funDecode = letterDecode + (26 - (offsetDecode) % 26);
+      console.log('a', funDecode);
+      if (funDecode > 90) {
+        funDecode = funDecode % 90 + 64;
+      }
+      console.log('b', funDecode);
       letterResDecode = String.fromCharCode(funDecode);
       finalMessageDecode = finalMessageDecode + letterResDecode;
     }
